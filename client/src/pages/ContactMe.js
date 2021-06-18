cfd58649ed18758acf6d102aaea21f75ef31a1b1
Cali0707 <calumramurray@gmail.com>
2021-06-18 18:14:30 -0400

    Got router to work

---
+++ client/src/pages/ContactMe.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+import "./ContactMe.css";
+import {useHistory} from "react-router-dom";
+
+export default function ContactMe () {
+    const history = useHistory();
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const []
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,8 +4,11 @@ import {Subtitle, TextSection, Title} from '../components/text/text';
 import TextInput from "../components/form/TextInput";
 import Button from "../components/form/Button";
 import Header from "../components/Header";
+import {useHistory} from "react-router-dom";
 
-export default function Home ({history}) {
+export default function Home () {
+    let history = useHistory();
+
     const handleClick = (link) => {
         history.push(link)
     }
@@ -13,7 +16,7 @@ export default function Home ({history}) {
     const imageLink = "https://images.unsplash.com/photo-1618143416214-16501f28d943?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
     return(
         <div className={"home"}>
-            <Header imageLink={imageLink} title={"Calum Murray"} />
+            <Header imageLink={imageLink} title={"Calum Murray"} history={history} isHome/>
             <div className={"about-me"}>
                 <img className={"profile"} src={"https://avatars.githubusercontent.com/u/65566908?v=4"} alt={"A picture of me"}/>
                 <div className={"about-me-text"}>
