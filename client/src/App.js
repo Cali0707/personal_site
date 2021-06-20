ab58363eeebf3924e31fff084809fc7b5031a419
Cali0707 <calumramurray@gmail.com>
2021-06-19 22:00:06 -0400

    Added timeline component

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,10 @@ import {BrowserRouter, Route, Redirect, Switch, useHistory} from "react-router-d
 import Home from "./pages/Home";
 import ProjectList from "./pages/ProjectList";
 import ContactMe from "./pages/ContactMe";
+import Timeline from "./components/Timeline";
 
 /* TODO
+- Make timeline component
 - Make My Interests page
 - Populate Projects page
 - Use local storage to prevent multiple emails (maybe also some kind of server side stop)
@@ -17,18 +19,93 @@ import ContactMe from "./pages/ContactMe";
 - Make Employment page
  */
 
+const events = [
+    {
+        title: 'Something here',
+        date: "July 07 2021",
+        info: "This is a paragraph with some test info"
+    },
+    {
+        title: 'Something else here',
+        date: "July 21 2021",
+        info: "This is a paragraph with some test info"
+    },
+    {
+        title: 'Some other event',
+        date: 'July 22 2021',
+        info: "This is a paragraph with some test info"
+    },
+    {
+        title: 'A test event',
+        date: 'July 24 2021',
+        info: "This is a paragraph with some test info"
+    },
+    {
+        title: 'Some other event',
+        date: 'July 22 2021',
+        info: "This is a paragraph with some test info"
+    },
+    {
+        title: 'A test event',
+        date: 'July 24 2021',
+        info: "This is a paragraph with some test info"
+    },
+    {
+        title: 'Some other event',
+        date: 'July 25 2021',
+        info: "This is a paragraph with some test info"
+    },
+    {
+        title: 'A test event',
+        date: 'July 26 2021',
+        info: "This is a paragraph with some test info"
+    },
+    {
+        title: 'Some other event',
+        date: 'July 27 2021',
+        info: "This is a paragraph with some test info"
+    },
+    {
+        title: 'A test event',
+        date: 'July 28 2021',
+        info: "This is a paragraph with some test info"
+    },
+    {
+        title: 'Some other event',
+        date: 'July 29 2021',
+        info: "This is a paragraph with some test info"
+    },
+    {
+        title: 'A test event',
+        date: 'July 30 2021',
+        info: "This is a paragraph with some test info"
+    },
+    {
+        title: 'Some other event',
+        date: 'July 31 2021',
+        info: "This is a paragraph with some test info"
+    },
+    {
+        title: 'A test event',
+        date: 'August 1 2021',
+        info: "This is a paragraph with some test info"
+    },
+]
+
 function App() {
 
     return (
-       <Switch>
-           {projects.map(proj => {
-               return(<Route path={proj.path} exact render={()=><ProjectPage project={proj}/>} />)
-           })}
-           <Route path={'/projects'} exact render={()=><ProjectList projects={projects} />} />
-           <Route path={'/contact'} component={ContactMe} />
-           <Route path={'/'} render={()=><Home />} />
+       // <Switch>
+       //     {projects.map(proj => {
+       //         return(<Route path={proj.path} exact render={()=><ProjectPage project={proj}/>} />)
+       //     })}
+       //     <Route path={'/projects'} exact render={()=><ProjectList projects={projects} />} />
+       //     <Route path={'/contact'} component={ContactMe} />
+       //     <Route path={'/'} render={()=><Home />} />
+       //
+       // </Switch>
 
-       </Switch>
+        <Timeline events={events}/>
     );
 }
 
---
+++ client/src/components/Timeline.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Timeline.css'
+import {Subtitle, TextSection} from "./text/text";
+
+export function TimelineItem({title, date, info}) {
+    const [isVisible, setVisible] = React.useState(true);
+    const domRef = React.useRef();
+    React.useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => setVisible(entry.isIntersecting));
+        });
+        const current = domRef.current;
+        observer.observe(current);
+        return () => observer.unobserve(current);
+    }, []);
+    return (
+        <div className={"timelineItem"}>
+            <div className={`timelineItemContent fadeInSection ${isVisible ? 'isVisible' : ''}`} ref={domRef}>
+                <Subtitle>{title}</Subtitle>
+                <time>{date}</time>
+                <TextSection>{info}</TextSection>
+                <span className={"circle"}/>
+            </div>
+        </div>
+    )
+}
+
+export default function Timeline({events}){
+    const timelineEvents = events.map((event, i) => (
+        <TimelineItem date={event.date} title={event.title} info={event.info} key={event.title+i.toString()}/>
+    ));
+    return(
+        events.length > 0 && (
+            <div className={"timelineContainer"}>
+                {timelineEvents}
+            </div>
+        )
+    )
+}
